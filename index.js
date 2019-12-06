abbb431f20ccb9c90706b55a8d1e0a83e98284af
hardy-awan <57611165+hardy-awan@users.noreply.github.com>
2019-12-06 21:42:37 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const app = express()
+
+mongoose.connect('mongodb://localhost/dilo')
+    .then(db => console.log('db connected'))
+    .catch(err => console.log(err));
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+app.use(express.static('public'));
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+        extended: true
+}))
+
+
+require('./router/route') (app)
+
+app.listen(9100, () => {
+    console.log('Berhasil menjalankan server pada port 9100')
+})
